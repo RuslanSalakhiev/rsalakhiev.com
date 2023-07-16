23a37fb67a3d787669d2bcedf6973db0e3ec36fd
rs <salakhiev.ruslan@gmail.com>
2023-07-16 22:26:00 +0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Home() {
+    return (
+        <>
+            <p>Hello, my name is Ruslan Salakhiev</p>
+            <p>I am a ...</p>
+        </>
+    );
+}
