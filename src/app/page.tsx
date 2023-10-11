f121b7ba27bdea98ee25c3f48492ae0f14a0b21a
rs <salakhiev.ruslan@gmail.com>
2023-10-11 13:34:11 +0400

    change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+'use client'
+
 import styles from './page.module.css';
+import Link from 'next/link';
+import { useState } from 'react';
+
 
 export default function Home() {
+
     return (
-        <>
-            <p>Hello, my name is Ruslan Salakhiev</p>
-            <p>I am a ...</p>
-        </>
+        <div className={`${styles.Container} `}>
+            <div className={`${styles.Title} `}>
+                <span className={styles.Title_Caption}>Hello, my name is Ruslan&nbsp;Salakhiev</span>
+                <span className={styles.Title_Caption}>I am <span className={styles.Title_Caption___text_gradient}>curios</span></span>
+            </div>
+            <div>
+                Projects: 
+                <ul>
+                    <li><Link href="/norm">Uzhe norm?</Link></li>
+                    <li><Link href="/styleroulette">Styleroulette</Link></li>
+                </ul>
+            </div>
+        </div>
     );
 }
