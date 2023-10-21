ac61ca3ebd34d7664f061c098aa6ed78c608661d
rs <salakhiev.ruslan@gmail.com>
2023-10-21 16:36:11 +0400

    curious

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
-'use client'
-
-import styles from './page.module.css';
 import Link from 'next/link';
-import { useState } from 'react';
 
 
 export default function Home() {
 
     return (
-        <div className={`${styles.Container} `}>
-            <div className={`${styles.Title} `}>
-                <span className={styles.Title_Caption}>Hello, my name is Ruslan&nbsp;Salakhiev</span>
-                <span className={styles.Title_Caption}>I am <span className={styles.Title_Caption___text_gradient}>curios</span></span>
+        <div className='p-10'>
+            <div className='flex flex-col'>
+                <span className='gradientTitle'>curios</span>
+                {/* <span> or Ruslan Salakhiev playground</span> */}
             </div>
-            <div>
+            {/* <div>
                 Projects: 
                 <ul>
                     <li><Link href="/norm">Uzhe norm?</Link></li>
                     <li><Link href="/styleroulette">Styleroulette</Link></li>
                 </ul>
-            </div>
+            </div> */}
         </div>
     );
 }
